1747c5e918f8e23ef823f7cd1ad123d84f1a50ff
powermate <218421152+powermate25@users.noreply.github.com>
2025-08-09 00:34:25 +0000

    Checkpoint save: Javascript exercise 06 Done (using loop, return & if .. else).

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(theString, theNumber) {
+
+    let userString = "";
+if (theNumber >= 0) {
+    for(i = 1; i <= theNumber; i++ ) {
+ userString += theString;
+    }
+    return userString; }
+    else {return "ERROR"}
 
 };
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
