d16bbfe05ef5f5137081fdb97436c5b555accc53
powermate <218421152+powermate25@users.noreply.github.com>
2025-08-29 22:55:55 +0000

    Checkpoint save: Javascript exercise 08_removeFromArray done.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (prm, ...index){
+/* console.log(prm.slice(...index)) */
+
+    return prm.filter(x => !index.includes(x))
+} ;
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
+
+
