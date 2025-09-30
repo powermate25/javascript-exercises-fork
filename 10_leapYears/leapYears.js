6d5c338ac5ab257b578c7400c4df16c2d993e5cc
powermate <218421152+powermate25@users.noreply.github.com>
2025-09-30 02:27:29 +0000

    Checkpoint save: Javascript exercise 10 LeapYears done

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,8 @@
-const leapYears = function() {
-
-};
+const leapYears = function(year) {
+  if ((year / 4) % 1 === 0 && Number.isInteger( (year/100) ) === false || Number.isInteger((year/400)) === true)
+     {return true}
+  else {return false}
+}
 
 // Do not edit below this line
 module.exports = leapYears;
