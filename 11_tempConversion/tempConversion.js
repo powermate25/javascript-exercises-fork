53cc42670fbf95ebc2c42273aba46c78433ee151
powermate <218421152+powermate25@users.noreply.github.com>
2025-09-30 11:41:48 +0000

    Checkpoint save: Javascript exercise 11 done.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farh) {
+  result = ((farh - 32) * (5/9) );
+  if( result > Math.floor(result) ) { return Number(result.toFixed(1))}
+ else {return result = Number(result)}
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  result = (cel * (9/5) + 32);
+ if( result > Math.floor(result) ) { return Number(result.toFixed(1))}
+ else {return result = Number(result)}
 };
 
 // Do not edit below this line
@@ -9,3 +15,4 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
