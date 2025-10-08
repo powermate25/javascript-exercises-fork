2a62157a1025539cc726477cd0926709a54bf456
powermate <218421152+powermate25@users.noreply.github.com>
2025-10-08 21:56:23 +0000

    Checkpoint save: Javascript exercise 15_getTheTitles done.

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(obj) {
+// for (let i in obj){ return ( obj[i]["title"] ) }
+ result = obj.map( i => i.title )
+ return result
 };
 
 // Do not edit below this line
