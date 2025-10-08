6a41b069558160d6ef8591d1ab5fb1ca14f15a8c
powermate <218421152+powermate25@users.noreply.github.com>
2025-10-08 03:01:31 +0000

    Checkpoint save: Javascript exercise 12_calculator done!

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+// calculator.sum( [7, 11]) ) toBe (18)
+
+const sum = function(arr) {
+	return arr.reduce( (total, item) => (total + item), 0 )
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce( (total, item) => (total * item) )
 };
 
-const power = function() {
-	
+// calculator.power(4, 3)).toBe(64); // 4 to third power is 64
+
+const power = function(num, power) {
+ let total = 1
+   for (i = 1; i <= power; i++){total *= num}
+   return total
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = num
+   for (i = 1; i <= num-1 ; i++){total *= i}  
+  if (total == 0) {return 1} else {return total}
 };
 
 // Do not edit below this line
