014cbd6ed0d6458c9307885fce1adc352f57574a
powermate <218421152+powermate25@users.noreply.github.com>
2025-10-08 21:23:45 +0000

    Checkpoint save: Javascript exercise 14_fibonacci done!

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+// Had to google for a reference formula on the web to adapt to this one :/
+// Guest there would be times when one will need to get inspired from somewhere.
 
+   if (num < 0) {return "OOPS"}
+   if (num == 0) {return 0}
+   let numberNum = Number(num)
+    let num1 = 1;
+    let num2 = 1;
+
+    let total;
+    if (num === 1) {
+        return num1;
+    } else if (num === 2) {
+        return num2;
+    } else {
+        for (let i = 3; i <= num; i++) {
+            total = num1 + num2;
+            num1 = num2;
+            num2 = total;
+        }
+        return num2;
+    }
 };
 
 // Do not edit below this line
